c5fdd806770cf80962507521e1db16869edb54f2
one0910 <one0910.yu@msa.hinet.net>
2023-09-19 16:18:20 +0800

    [增加] 在第一個視slider頻加入會員提示alert的功能

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,16 @@ import './styles.css';
 
 export default function App() {
   const swiperRef = useRef(null) as any;
-  console.log('navigator => ', navigator)
 
   useEffect(() => {
-    console.log('getDeviceType => ', getDeviceType())
     if (swiperRef.current) {
       const swiperInstance = swiperRef.current.swiper;
+      // console.log(' activeVideo.classList=> ', document.querySelector('.swiper-slide')?.classList)
       swiperInstance.on('slideChange', function () {
         const videos = document.querySelectorAll('video');
         videos.forEach(video => video.pause());
         setTimeout(() => {
+          console.log(' activeVideo.classList2=> ', document.querySelector('.swiper-slide')?.classList)
           const activeVideo = document.querySelector('.swiper-slide-active video') as HTMLVideoElement;
           if (activeVideo && getDeviceType() !== 'iPhone') {
             activeVideo.play()
@@ -41,7 +41,7 @@ export default function App() {
       >
         {['Audi_A4_S4', 'Bugatti_Chiron', 'Range_Rover_Sport_L322', 'Rolls_Royce_Ghost', 'Toyota_Camry_XV70', 'Volkswagen_Golf_7'].map((link, index) => {
           return (
-            <SwiperSlide key={index} >
+            <SwiperSlide key={index}>
               <Video {...{ link }} />
             </SwiperSlide>
           )
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -10,12 +10,38 @@ interface VideoProps {
 const Video: React.FC<VideoProps> = (props) => {
   const { link } = props;
   const videoRef = useRef<HTMLVideoElement>(null);
+  const videoContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     const video = videoRef.current;
+    const isActiveClass = videoContainerRef.current?.parentElement?.classList[1]
     let plyr: Plyr | null = null;
 
+    const checkPlayTime = () => {
+      if (plyr && isActiveClass === "swiper-slide-active") {
+        const currentTime = plyr.currentTime; // 獲取當前播放時間
+        if (currentTime > 5) {
+          plyr.pause();
+          const videoContainer = document.querySelector('.videoContainer');
+          if (videoContainer) {
+            const overlay = document.createElement('div');
+            overlay.style.position = 'absolute';
+            overlay.style.top = '0';
+            overlay.style.left = '0';
+            overlay.style.width = '100%';
+            overlay.style.height = '100%';
+            overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
+            overlay.style.zIndex = '1000';
+            overlay.innerText = '需加入會員才能繼續觀看';
+            videoContainer.appendChild(overlay);
+          }
+          alert("需加入會員才能繼續觀看");
+        }
+      }
+    }
+
     if (Hls.isSupported() && video) {
+
       const hls = new Hls();
       hls.loadSource(`./videos/${link}.m3u8`);
       hls.attachMedia(video);
@@ -39,6 +65,7 @@ const Video: React.FC<VideoProps> = (props) => {
         //   'fullscreen',
         // ]
       });
+      plyr.on('timeupdate', checkPlayTime)
 
     } else if (video && video.canPlayType("application/vnd.apple.mpegurl")) {
       video.src = `./videos/${link}.m3u8`;
@@ -62,11 +89,20 @@ const Video: React.FC<VideoProps> = (props) => {
         //   'fullscreen',
         // ]
       });
+      console.log('currentTime => ', plyr.currentTime)
+      plyr.on('timeupdate', checkPlayTime)
     }
+
+    // return () => {
+    //   if (plyr) {
+    //     plyr.off('timeupdate', checkPlayTime);
+    //   }
+    // };
+
   }, []);
 
   return (
-    <div className="videoContainer">
+    <div className="videoContainer" ref={videoContainerRef}>
       <video ref={videoRef} controls={true} poster={`./images/${link}.png`} playsInline />
       {/* <video ref={videoRef} controls={true} playsInline autoPlay={true} /> */}
     </div>
